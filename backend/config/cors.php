a07958df7737377a1c2c5a7f6d523dda80d09c7b
2AM <acha.arthur@gmail.com>
2025-04-29 22:14:58 -0300

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,17 +14,17 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173', 'https://front-production-2f2b.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
