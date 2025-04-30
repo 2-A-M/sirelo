2b91bad2a7f8b462dc690456a0d80a90e80be6d6
2AM <acha.arthur@gmail.com>
2025-04-30 11:17:22 -0300

    dark mode como padrao

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Configura o dark mode como padrão
+document.documentElement.classList.add('dark');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
